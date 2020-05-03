e51286b412c479bcc4fcd17e0a5ab9ae43c53e50
Shubham Soni <shubhamsoni@Shubhams-MacBook-Pro.local>
2020-05-04 00:55:53 +0530

    pycaret for regression for housePriceprediction

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,16 +1,23 @@
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 
 
-class Vizualize:
+class Visualize:
     '''All kinds of plot'''
 
     def __init__(self, data, target):
+        '''
+        Args:
+            data: training data
+            target: target name
+        '''
         self.df = data
         self.target = target
-
-
+    import pandas as pd
+    pd.options.display.max_columns = None
+    pd.options.display.max_rows = None
+    
     def scatter(self, x, y, **kwargs):
         plt.subplots()
-        plt.scatter(x, y, **kwargs)
+        plt.scatter(self.df[x], self.df[y], **kwargs)
         plt.show()
-
